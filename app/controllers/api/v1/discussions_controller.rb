61a69dbf87e46c54f37e7980a7e00eeb7aebe295
github username <jhernandez1692@gmail.com>
2021-02-18 17:14:43 -0500

    Create and Index actions for discussion.

---
+++ app/controllers/api/v1/discussions_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::DiscussionsController < ApplicationController
+
+    def index
+        discussion = Discussion.all
+        render json: discussion
+    end
+
+    def create
+        discussion = Discussion.new(discussion_params)
+        
+        if discussion.save
+            render json: discussion, status: :accepted
+        else
+            render json: {errors: discussion.errors.full_messages}, status: :unprocessable_entity
+        end
+        
+    end
+
+    private
+
+    def discussion_params
+        params.require(:discussion).permit(:title, :user_id, :board_id)
+    end
+
+end

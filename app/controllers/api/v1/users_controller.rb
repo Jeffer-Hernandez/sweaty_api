b72e9779ca84d9921618fffae8f2a94ec9f01412
github username <jhernandez1692@gmail.com>
2021-02-18 17:29:48 -0500

    started some actions for users.

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::UsersController < ApplicationController
+    
+    def index
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def user_params
+    end
+    
+end
